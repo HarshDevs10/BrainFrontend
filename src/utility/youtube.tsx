b5f31ee510a9dcdcba6d20fe275cba76e853b04c
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-10-28 17:41:10 +0000

    Removed unneed Loading animations

--- src/utility/youtube.tsx
+++ src/utility/youtube.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react"
 import { Iframe } from "./iframe"
 
 interface youtubeProps{
@@ -6,16 +5,6 @@ interface youtubeProps{
 }
 
 export const Youtubing = (prop: youtubeProps) => {
-    const [ready, setReady] = useState<boolean>(false)
-
-    useEffect(() => {
-        const timer = setTimeout(() => setReady(true), 2000)
-        return () => clearTimeout(timer)
-    }, [prop.url])
-
-    if(!ready){
-        return <div className="text-center">⏳ Preparing video...</div>
-    }
 
     if(prop.url.includes("embed")){
         return <iframe className="aspect-video justify-center"
