b546f0ad22f23b8160ef7a746cd4c4cc2f3ff64b
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2026-04-22 12:36:57 +0000

    Fixed search bar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -115,8 +115,49 @@ const handleSearch = () => {
 
 </div>
         <div className="flex justify-end-safe gap-4 pt-5 pb-9">
+          <div className="flex gap-3 pt-5 items-center">
+              <div className="flex gap-3 items-center">
+                <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Search by tags (ai, tech)"
+                  value={searchTags}
+                  onChange={(e) => setsearchTags(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      handleSearch();
+                    }
+                  }}
+                  className="border p-2 rounded w-64 pr-10"
+                />
+
+                {searchTags && (
+                  <span
+                    onClick={() => {
+                      setsearchTags("");
+                      setCardsec(AllCard);
+                      setNoResult(false);
+                    }}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-black"
+                  >
+                    ✕
+                  </span>
+                )}
+              </div>
+
+              <button
+                onClick={handleSearch}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Search
+              </button>
+            </div>
+          </div>
           {/*<Button variant="Secondary" fullWidth="no" size="lg" text="Share Brain" onClick={() => {}} stratIcon={<ShareIcon size="lg"/>}/>*/}
-          <Button variant="Primary" fullWidth="no" size="lg" text="Add Content" onClick={() => {setOpen(true)}} stratIcon={<PlusIcon size="lg"/>}/>
+          <div className="flex justify-end-safe gap-4 pt-5 pb-9">
+            <Button variant="Primary" fullWidth="no" size="lg" text="Add Content" onClick={() => {setOpen(true)}} stratIcon={<PlusIcon size="lg"/>}/>
+          </div>
+          {/*<Button variant="Primary" fullWidth="no" size="lg" text="Add Content" onClick={() => {setOpen(true)}} stratIcon={<PlusIcon size="lg"/>}/> */}
         </div>
         {noResult ? (
   <div className="text-center text-gray-500 mt-10 text-lg">
